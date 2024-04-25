aa78f229b57d2d0256526a58210bbe7df9393fd8
Koen Luyten <kluyten@xpirit.com>
2024-04-25 14:48:19 +0200

    bla

---
+++ sol2/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = Age;        
+    }
+}
